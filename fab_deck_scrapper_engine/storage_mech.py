d20d6cc319e1aaabf314e932a97ea6cc1c5d5eab
htvan88 <101529560+htvan88@users.noreply.github.com>
2022-03-16 23:22:16 -0700

    added headers to flat file

--- fab_deck_scrapper_engine/storage_mech.py
+++ fab_deck_scrapper_engine/storage_mech.py
@@ -1,7 +1,10 @@
 from decklist_metadata import DecklistMetadata
 import json
 
+
+# temp storage to flat file until database is set up
 def store_metadata(metadata_list:list):
     with open('data_store.txt', 'w') as file:
+        file.write(DecklistMetadata.print_headers() + '\n')
         for decklist_metadata in metadata_list:
             file.write(decklist_metadata.print_str() + '\n')
