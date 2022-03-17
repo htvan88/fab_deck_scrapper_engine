432418d340f4c8c58c7f2a400b0e9fa0a5e7e227
htvan88 <101529560+htvan88@users.noreply.github.com>
2022-03-16 23:15:41 -0700

    added formatting utils

--- fab_deck_scrapper_engine/util.py
+++ fab_deck_scrapper_engine/util.py
@@ -1,4 +1,25 @@
+from datetime import datetime
+
 
 def deck_url_creator(data:str) -> str:
     url = "https://fabtcg.com/decklists/" + data.replace(" ", "-").replace(".", "").lower()
     return url
+
+
+def rank_as_int(rank:str) -> int:
+    if rank != 'None':
+        return int(rank[0])
+    else: return ''
+
+# format is embedded in the data, 
+# check to see if keywords are present else return ''
+def get_event_type(data:str) -> str:
+    if data.find('proquest'):
+        return 'proquest'
+    elif data.find('armory'):
+        return 'armory'
+    else:
+        return '' 
+
+def string_to_date(date:str) -> datetime:
+    return datetime.strptime(date, '%d %b %Y')
